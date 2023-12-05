dd7224073177a8aa726c71f7d97ae3fe9e2999d2
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-04 18:43:57 -0700

    4 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const mainRemove = document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory')
+
+const headerText = document.getElementById("victory");
+
+newHeader.textContent = "Big Bill is the champion"
+
+document.body.append(newHeader);
+
+/*
+const mainRemove = document.querySelector("main").remove();
+*/
